47dca0d264ba59c389d09f144d2af52f1839c55a
Patricio Cainzo Pereira <pcainzo@live.com.ar>
2024-05-03 14:47:24 -0300

    Se agrego el manejo de errores para los controladores :D

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,20 +1,39 @@
-const Users= require('./../models/User')
+const Users= require('../models/User')
+const {httpError}=require('../helpers/handleError')
 const user = {
     list: async(req,res)=>{
-        const users= await Users.find()
-        res.status(200).send(users)
+        try{
+            const listAll = await Users.find({})
+            res.send({data : listAll})
+        }
+        catch(e){
+            httpError(res,e)
+        }
     },
     get: async(req,res)=>{
-        const {id} = req.params
-        const user = await Users.find({_id:id})
-        res.status(200).send(user)
-        },
+        try {
+            const { id } = req.params;
+            const user = await Users.findById(id);
+            if (!user) {
+                return res.status(404).send({ message: "Usuario no encontrado" });
+            }
+            res.status(200).send(user);
+        } catch (e) {
+            httpError(res, e);
+        }
+    },
    
     create : async (req,res)=>{
-        console.log(req.body)
-        const user = new Users(req.body)
-        const SavedUser= await user.save()
-        res.status(201).send(SavedUser._id)
+        try{
+            const {username , password , name , lastname, email , dni , fechaDeNacimiento , telefono , codigoPostal}= req.body
+            const restDetail = await Users.reate({
+                username , password , name , lastname, email , dni , fechaDeNacimiento , telefono , codigoPostal
+            })
+            res.send({data : restDetail})
+        }
+        catch(e){
+            httpError(res,e)
+        }
     },
     update: async (req, res) => {
         const { id } = req.params;
@@ -28,9 +47,29 @@ const user = {
             // Guarda el usuario actualizado en la base de datos
             await user.save();
             res.sendStatus(204);
-        } catch (error) {
-            console.error(error);
-            res.status(500).send("Error al actualizar usuario");
+        } catch (e) {
+            httpError(res,e)
+        }
+    },
+    modify: async (req, res) => {
+        const { id } = req.params;
+        try {
+            const user = await Users.findById(id);
+            if (!user) {
+                return res.status(404).send("Usuario no encontrado");
+            }
+    
+            // Actualiza solo las propiedades proporcionadas en req.body
+            Object.keys(req.body).forEach(key => {
+                user[key] = req.body[key];
+            });
+    
+            // Guarda el usuario actualizado en la base de datos
+            await user.save();
+            
+            res.sendStatus(204);
+        } catch (e) {
+            httpError(res, e);
         }
     },
     
--- app/routes/user.js
+++ app/routes/user.js
@@ -6,6 +6,7 @@ router.get('/', user.list)
 router.get('/:id', user.get)
 router.post('/', user.create)
 router.put('/:id', user.update)
+router.patch('/:id', user.modify)
 router.delete('/:id', user.delete)
 
 module.exports=router
