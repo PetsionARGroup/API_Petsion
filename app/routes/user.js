a6d7847fa7c533c6b834bc22cca00ccba95ea75b
Patricio Cainzo Pereira <pcainzo@live.com.ar>
2024-05-16 10:44:56 -0300

    volviendo atras

--- app/routes/user.js
+++ app/routes/user.js
@@ -3,11 +3,10 @@ const router = express.Router()
 const user = require('../controllers/user.controller')
 const {validateJwt} = require ('../middleware/session')
 
-router.post('/register',user.register)
-router.post('/login', user.login)
-
 router.get('/', user.list)
 router.get('/:id', user.get)
+router.post('/register',user.register)
+router.post('/login', user.login)
 router.put('/:id',validateJwt, user.update)
 router.patch('/:id',validateJwt, user.modify)
 router.delete('/:id',validateJwt, user.delete)
